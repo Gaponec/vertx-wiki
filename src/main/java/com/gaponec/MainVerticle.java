daaf842f7dfffb673c3dd503dff75f3ace86498e
Bohdan Liakhovetskyi <bohdan.liakhovetskyi-external@sema4genomics.com>
2020-02-02 11:54:58 +0200

    routing addition

--- src/main/java/com/gaponec/MainVerticle.java
+++ src/main/java/com/gaponec/MainVerticle.java
@@ -3,17 +3,26 @@ package com.gaponec;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   private JDBCClient jdbcClient;
+  private FreeMarkerTemplateEngine templateEngine;
 
   private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages " +
     "(Id integer identity primary key, Name varchar(255) unique, Content clob)";
@@ -37,7 +46,83 @@ public class MainVerticle extends AbstractVerticle {
 
   private Future<Void> createHttpServer() {
     Promise<Void> promise = Promise.promise();
+    HttpServer httpServer = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::indexHandler);
+    router.get("/wiki/:page").handler(this::pageRenderingHandler);
+    router.post().handler(BodyHandler.create());
+    router.post("/save").handler(this::pageUpdateHandler);
+    router.post("/create").handler(this::pageCreateHandler);
+    router.post("/delete").handler(this::pageDeletionHandler);
+
+    templateEngine = FreeMarkerTemplateEngine.create(vertx);
+
+    httpServer
+      .requestHandler(router)
+      .listen(8080, asyncResult -> {
+        if (asyncResult.succeeded()) {
+          LOGGER.info("Http server running on port 8080");
+          promise.complete();
+        } else {
+          LOGGER.error("Could not start http server", asyncResult.cause());
+          promise.fail(asyncResult.cause());
+        }
+      });
+
     return promise.future();
+  }
+
+  private void pageDeletionHandler(RoutingContext routingContext) {
+    jdbcClient.getConnection(connectionAsyncResult -> {
+      if (connectionAsyncResult.succeeded()) {
+        SQLConnection connection = connectionAsyncResult.result();
+        connection.query(SQL_ALL_PAGES, result -> {
+          connection.close();
+
+          if (result.succeeded()) {
+            List<String> pages = result.result()
+              .getResults()
+              .stream()
+              .map(json -> json.getString(0))
+              .sorted()
+              .collect(Collectors.toList());
+
+            routingContext.put("title", "Wiki home");
+            routingContext.put("pages", pages);
+            templateEngine.render(routingContext.data(), "templates/index.ftl", asyncResult -> {
+              if (asyncResult.succeeded()) {
+                routingContext.response().putHeader("Context-Type", "text/html");
+                routingContext.response().end(asyncResult.result());
+              } else {
+                routingContext.fail(asyncResult.cause());
+              }
+            });
+          } else {
+            routingContext.fail(result.cause());
+          }
+
+        });
+      } else {
+        routingContext.fail(connectionAsyncResult.cause());
+      }
+    });
+  }
+
+  private void pageCreateHandler(RoutingContext routingContext) {
+
+  }
+
+  private void pageUpdateHandler(RoutingContext routingContext) {
+
+  }
+
+  private void pageRenderingHandler(RoutingContext routingContext) {
+
+  }
+
+  private void indexHandler(RoutingContext routingContext) {
+
   }
 
   private Future<Void> prepareDatabase() {
