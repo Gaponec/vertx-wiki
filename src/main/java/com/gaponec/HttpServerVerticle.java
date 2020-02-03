02f2b9833084882e44b2505af5374611d0e64316
Liakhovetskyi Bohdan <uaGaponec@gmail.com>
2020-02-03 20:24:10 +0200

    refactoring to difference verticle: HttpServerVerticle - index handler

--- src/main/java/com/gaponec/HttpServerVerticle.java
+++ src/main/java/com/gaponec/HttpServerVerticle.java
@@ -2,7 +2,9 @@ package com.gaponec;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -15,12 +17,18 @@ public class HttpServerVerticle extends AbstractVerticle {
   private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
 
   private static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+  public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
   private static final Integer DEFAULT_HTTP_SERVER_PORT = 8080;
+
+  private String wikiDbQueue = "wikidb.queue";
 
   private FreeMarkerTemplateEngine templateEngine;
 
   @Override
   public void start(Promise<Void> promise) {
+
+    wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue");
+
     HttpServer httpServer = vertx.createHttpServer();
 
     Router router = Router.router(vertx);
@@ -64,5 +72,24 @@ public class HttpServerVerticle extends AbstractVerticle {
   }
 
   private void indexHandler(RoutingContext routingContext) {
+    DeliveryOptions deliveryOptions = new DeliveryOptions().addHeader("action", "all-pages");
+
+    vertx.eventBus().request(wikiDbQueue, new JsonObject(), deliveryOptions, reply -> {
+      if (reply.succeeded()) {
+        JsonObject body = (JsonObject) reply.result().body();
+        routingContext.put("title", "wiki home");
+        routingContext.put("pages", body.getJsonArray("pages").getList());
+        templateEngine.render(routingContext.data(), "templates/index.ftl", asyncResultBuffer -> {
+          if (asyncResultBuffer.succeeded()) {
+            routingContext.response().putHeader("Content-Type", "text/html");
+            routingContext.response().end(asyncResultBuffer.result());
+          } else {
+            routingContext.fail(asyncResultBuffer.cause());
+          }
+        });
+      } else {
+        routingContext.fail(reply.cause());
+      }
+    });
   }
 }
